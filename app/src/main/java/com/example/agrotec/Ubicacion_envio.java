f72447eed9ac71a5a3cc18e5221cd7b076428dd4
Jhae <jhonatan9818@gmail.com>
2019-10-10 13:02:27 -0500

    Casi todas las vistas

---
+++ app/src/main/java/com/example/agrotec/Ubicacion_envio.java
@@ -0,0 +1,14 @@
+package com.example.agrotec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ubicacion_envio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubicacion_envio);
+    }
+}
---
+++ app/src/main/java/com/example/agrotec/metodo_adquisicion.java
@@ -0,0 +1,14 @@
+package com.example.agrotec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class metodo_adquisicion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metodo_adquisicion);
+    }
+}
